d97f62c8fcaf3ff07ff59fccc1a12af5d4287ef7
RichardNWeb <richardnguyenatwork@gmail.com>
2023-02-22 15:50:07 -0500

    add quick game

--- fhs/src/views/gamemodeMenu/QuickGame.vue
+++ fhs/src/views/gamemodeMenu/QuickGame.vue
@@ -13,20 +13,103 @@
 <div class="container text-center">
        <div class="row align-items-start">
         <form class="away col">
-          <select >
+          <select class="teams" v-model="selectedAwayTeam">
+            <option disabled value ="">Select a team </option>
+            <option>Anaheim Ducks</option>
+            <option>Arizona Coyotes</option>
+            <option>Boston Bruins</option>
+            <option>Buffalo Sabres</option>
+            <option>Calgary Flames</option>
+            <option>Carolina Hurricanes</option>
+            <option>Chicago Blackhawks</option>
+            <option>Colorado Avalanche</option>
+            <option>Columbus Blue Jackets</option>
+            <option>Dallas Stars</option>
+            <option>Detroit Red Wings</option>
+            <option>Edmonton Oilers</option>
+            <option>Florida Panthers</option>
+            <option>Los Angeles Kings</option>
+            <option>Minnesota Wild</option>
+            <option>Montreal Canadiens</option>
+            <option>Nashville Predators</option>
+            <option>New Jersey Devils</option>
+            <option>New York Islanders</option>
+            <option>New York Rangers</option>
+            <option>Ottawa Senators</option>
+            <option>Philadelphia Flyers</option>
+            <option>Pittsburgh Penguins</option>
+            <option>San Jose Sharks</option>
+            <option>Seattle Kraken</option>
+            <option>St Louis Blues</option>
+            <option>Tampa Bay Lightning</option>
+            <option>Toronto Maple Leafs</option>
+            <option>Vancouver Canucks</option>
+            <option>Vegas Golden Knights</option>
+            <option>Washington Capitals</option>
+            <option>Winnipeg Jets</option>
+            
+          </select>
+
+          <!-- Year Options -->
+          <select>
             <option>
-              Anaheim Ducks
+              Current Year
             </option>
           </select>
+      
+        </form>
+        
+        <p>Away: {{ selectedAwayTeam }} </p>
+        <form class="home col">
+          <select class="teams" v-model="selectedHomeTeam">
+            <option disabled value ="">Select a team </option>
+            <option>Anaheim Ducks</option>
+            <option>Arizona Coyotes</option>
+            <option>Boston Bruins</option>
+            <option>Buffalo Sabres</option>
+            <option>Calgary Flames</option>
+            <option>Carolina Hurricanes</option>
+            <option>Chicago Blackhawks</option>
+            <option>Colorado Avalanche</option>
+            <option>Columbus Blue Jackets</option>
+            <option>Dallas Stars</option>
+            <option>Detroit Red Wings</option>
+            <option>Edmonton Oilers</option>
+            <option>Florida Panthers</option>
+            <option>Los Angeles Kings</option>
+            <option>Minnesota Wild</option>
+            <option>Montreal Canadiens</option>
+            <option>Nashville Predators</option>
+            <option>New Jersey Devils</option>
+            <option>New York Islanders</option>
+            <option>New York Rangers</option>
+            <option>Ottawa Senators</option>
+            <option>Philadelphia Flyers</option>
+            <option>Pittsburgh Penguins</option>
+            <option>San Jose Sharks</option>
+            <option>Seattle Kraken</option>
+            <option>St Louis Blues</option>
+            <option>Tampa Bay Lightning</option>
+            <option>Toronto Maple Leafs</option>
+            <option>Vancouver Canucks</option>
+            <option>Vegas Golden Knights</option>
+            <option>Washington Capitals</option>
+            <option>Winnipeg Jets</option>
+            
+          </select>
+
+          <!-- Year Options -->
+        
+          <select>
+            <option>
+              Current Year
+            </option>
+          </select>
+       
+
         </form>
 
-        <form class="home col">
-          <select >
-            <option>
-              Anaheim Ducks
-            </option>
-          </select>
-        </form>
+        <p>Home: {{ selectedHomeTeam }} </p>
 </div>
 </div>
     
@@ -45,6 +128,20 @@
     </div>
   </template>
   
-  <script></script>
+  <script>
+export default{
+  data: function(){
+    return{
+      selectedAwayTeam: "",
+      selectedHomeTeam: ""
+    }
+  }
+}
+
+  </script>
   
-  <style></style>
+  <style>
+    .teams{
+    
+    }
+  </style>
